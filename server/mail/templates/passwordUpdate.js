96eacb6154b889f805f870c6ac48a3870c579c77
Abhishek Morya <abhimorya8826@gmail.com>
2025-06-12 11:01:11 +0530

    done

--- server/mail/templates/passwordUpdate.js
+++ server/mail/templates/passwordUpdate.js
@@ -56,7 +56,7 @@ exports.passwordUpdated = (email, name) => {
     <body>
         <div class="container">
             <a href="https://mind-link-an-edtech-platform.vercel.app"><img class="logo"
-                    src="https://mind-link-an-edtech-platform.vercel.app/rzp_logo.png" alt="StudyNotion Logo"></a>
+                    src="https://mind-link-an-edtech-platform.vercel.app/rzp_logo.png" alt="MindLink Logo"></a>
             <div class="message">Password Update Confirmation</div>
             <div class="body">
                 <p>Hey ${name},</p>
